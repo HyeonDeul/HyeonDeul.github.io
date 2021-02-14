f52bb468d9b29a6fe8b947aae63f6a8d37235c8c
HyeonDeul <78081827+HyeonDeul@users.noreply.github.com>
2021-02-14 13:48:08 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -5,8 +5,8 @@ const IMG_NUMBER = 3;
 
 function paintImage(imgNumber){
     const image = new Image();
-    // image.src = `images/${imgNumber+1}.jpg`;
-    image.src = `https://github.com/HyeonDeul/HyeonDeul.github.io/blob/main/Images/${imgNumber+1}.jpg`;
+    image.src = `images/${imgNumber+1}.jpg`;
+    // image.src = `https://github.com/HyeonDeul/HyeonDeul.github.io/blob/main/Images/${imgNumber+1}.jpg`;
     image.classList.add("bgImage");
     body.prepend(image);
 }
